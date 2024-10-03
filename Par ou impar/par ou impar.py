f8ce3a7e9b51191fce095ffeff15048f8a8bc46d
Lívia Vilhena <liviavilhena@outlook.com>
2024-10-02 22:23:22 -0300

    Add files via upload

---
+++ Par ou impar/par ou impar.py
@@ -0,0 +1,9 @@
+#Escolha do usuário
+numero_escolhido = int(input("\nDigite um número para verificação: "))
+
+#Condicional de verificação
+if numero_escolhido % 2 == 0:
+    print("\nEsse número é par\n")
+else:
+    print("\nEsse número é impar\n")
+    
